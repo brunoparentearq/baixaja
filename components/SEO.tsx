b485f14eb6a70380e1a825748fcc97ea8ece2a32
brunoparentearq <brunoparentearq@gmail.com>
2026-01-19 03:59:46 -0300

    Update SEO.tsx

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,8 +1,23 @@
-export default function SEO({ name, description }: { name: string, description: string }) {
+export default function SEO({ 
+  name, 
+  description, 
+  category, 
+  operatingSystem, 
+  image, 
+  slug 
+}: { 
+  name: string; 
+  description: string;
+  category?: string;
+  operatingSystem?: string;
+  image?: string;
+  slug?: string;
+}) {
   return (
     <>
       <title>{`${name} - Baixa Já Downloads`}</title>
       <meta name="description" content={description} />
+      {category && <meta name="category" content={category} />}
     </>
   );
 }
